d50fca661b2db5dc3ffce0a2976c6aaf474ec296
Masirika Constantin <elconstant73@gmail.com>
2025-05-16 20:10:52 +0100

    DaisyUI

--- resources/views/livewire/futurist-sales-dashboard.blade.php
+++ resources/views/livewire/futurist-sales-dashboard.blade.php
@@ -241,7 +241,7 @@
     
         <!-- Modal Détails de vente -->
         @if($showDetailsModal)
-        <div class="fixed inset-0 z-50 overflow-y-auto rounded rounded-2xl">
+        <div class="fixed inset-0 z-50 overflow-y-auto  rounded-2xl">
             <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div class="fixed inset-0 transition-opacity" aria-hidden="true">
                     <div class="absolute inset-0 bg-gray-900 opacity-90" wire:click="hideDetails"></div>
