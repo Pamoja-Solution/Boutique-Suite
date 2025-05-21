1694a3312977b11cefa1c7b4818c0a55bcd87dbc
Masirika Constantin <elconstant73@gmail.com>
2025-05-21 23:26:23 +0100

    modification produit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -35,15 +35,15 @@
          </style>
          <link href="/css/app.css" rel="stylesheet">
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans bg-base-100  antialiased">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
             <div>
                 <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-20 h-20 fill-current " />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-base-300 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Password;
+use App\Models\User; // N'oubliez pas d'importer le modèle User
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
@@ -17,27 +18,30 @@ new #[Layout('layouts.guest')] class extends Component
             'email' => ['required', 'string', 'email'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
+        // Désactiver le compte utilisateur avant d'envoyer le lien
+        $user = User::where('email', $this->email)->first();
+        
+        if ($user) {
+            $user->update(['status' => 0]); // Désactive le compte
+        }
+
+        // Envoi du lien de réinitialisation
         $status = Password::sendResetLink(
             $this->only('email')
         );
 
         if ($status != Password::RESET_LINK_SENT) {
             $this->addError('email', __($status));
-
             return;
         }
 
         $this->reset('email');
 
-        session()->flash('status', __($status));
+        // Message personnalisé pour informer l'utilisateur
+        session()->flash('status', __($status).' '.__('Votre compte a été temporairement désactivé pour des raisons de sécurité. Veuillez contacter l\'administrateur pour le réactiver.'));
     }
-}; ?>
-
-<div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+}; ?><div>
+    <div class="mb-4 text-sm ">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
@@ -58,4 +62,22 @@ new #[Layout('layouts.guest')] class extends Component
             </x-primary-button>
         </div>
     </form>
+
+    <!-- Ajout d'un message d'information supplémentaire -->
+    @if(session('status'))
+        <div class="mt-4 p-4 bg-yellow-50 border-l-4 border-yellow-400">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="ml-3">
+                    <p class="text-sm text-yellow-700">
+                        {{ __('Pour des raisons de sécurité, votre compte a été temporairement désactivé. Veuillez contacter l\'administrateur pour le réactiver après avoir changé votre mot de passe.') }}
+                    </p>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
